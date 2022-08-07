f22e67e6867ab6aca474ee7a642db7ab4ab7ae81
kit <tingiao.ki@ttpsc.com>
2022-08-07 23:01:26 +0800

    Update syntax because of upgrade of compiler.

--- ethereum/deploy.js
+++ ethereum/deploy.js
@@ -1,23 +1,25 @@
-const HDWalletProvider = require('@truffle/hdwallet-provider');
-const Web3 = require('web3');
-const compiledFactory = require('../ethereum/build/CampaignFactory.json')
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const Web3 = require("web3");
+const compiledFactory = require("./build/CampaignFactory.json");
 
 const provider = new HDWalletProvider(
-  'mixture mandate motion always mom exclude ready fantasy junior silent merge twenty',
-  'https://rinkeby.infura.io/v3/194b595104124ffcbf2ad5f2e02536e1'
+  "YOUR_MNEMONIC",
+  // remember to change this to your own phrase!
+  "YOUR_INFURA_URL"
+  // remember to change this to your own endpoint!
 );
 const web3 = new Web3(provider);
 
 const deploy = async () => {
   const accounts = await web3.eth.getAccounts();
 
-  console.log('Attempting to deploy from account', accounts[0]);
+  console.log("Attempting to deploy from account", accounts[0]);
 
-  const result = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
-    .deploy({ data: compiledFactory.bytecode })
-    .send({ gas: '1000000', from: accounts[0] });
+  const result = await new web3.eth.Contract(compiledFactory.abi)
+    .deploy({ data: compiledFactory.evm.bytecode.object })
+    .send({ gas: "1400000", from: accounts[0] });
 
-  console.log('Contract deployed to', result.options.address);
+  console.log("Contract deployed to", result.options.address);
   provider.engine.stop();
 };
 deploy();
--- pages/campaigns/requests/index.js
+++ pages/campaigns/requests/index.js
@@ -1,72 +1,73 @@
 import React, { Component } from "react";
 import { Button, Table } from "semantic-ui-react";
 import { Link } from "../../../routes";
-import Layout from "../../../Components/Layout";
+import Layout from "../../../components/Layout";
 import Campaign from "../../../ethereum/campaign";
-import RequestRow from "../../../Components/RequestRow";
+import RequestRow from "../../../components/RequestRow";
 
 class RequestIndex extends Component {
-    static async getInitialProps(props) {
-        const { address } = props.query;
-        const campaign = Campaign(address);
-        const requestsCount = await campaign.methods.getRequestsCount().call();
-        const approversCount = await campaign.methods.approversCount().call();
+  static async getInitialProps(props) {
+    const { address } = props.query;
+    const campaign = Campaign(address);
+    const requestCount = await campaign.methods.getRequestsCount().call();
+    const approversCount = await campaign.methods.approversCount().call();
 
+    const requests = await Promise.all(
+      Array(parseInt(requestCount))
+        .fill()
+        .map((element, index) => {
+          return campaign.methods.requests(index).call();
+        })
+    );
 
-        //Array(requestsCount).fill()，為使用Array建構子建立requestsCount值長度的陣列，並將所有元素初始化為undefined
-        const requests = await Promise.all(
-            Array(parseInt(requestsCount)).fill().map((element, index) => {
-                return campaign.methods.requests(index).call();
-            })
-        );
+    return { address, requests, requestCount, approversCount };
+  }
 
-        return { address, requests, requestsCount, approversCount };
-    }
+  renderRows() {
+    return this.props.requests.map((request, index) => {
+      return (
+        <RequestRow
+          key={index}
+          id={index}
+          request={request}
+          address={this.props.address}
+          approversCount={this.props.approversCount}
+        />
+      );
+    });
+  }
 
-    renderRows() {
-        return this.props.requests.map((request, index) => {
-            return <RequestRow
-                key={ index }
-                id={ index }
-                request={ request }
-                address={ this.props.address }
-                approversCount={ this.props.approversCount }
-            />
-        });
-    }
+  render() {
+    const { Header, Row, HeaderCell, Body } = Table;
 
-    render() {
-         //ES2015 語法糖，直接將所有table標籤的屬性作為變數提取。
-        const { Header, Row, HeaderCell, Body } = Table;
-
-        return (
-            <Layout>
-                <h3>List</h3>
-                <Link route={`/campaigns/${this.props.address}/requests/new`}>
-                    <a>
-                        <Button primary floated="right" style={{ marginBottom: 10 }}>Add Request</Button>
-                    </a>
-                </Link>
-                <Table>
-                    <Header>
-                        <Row>
-                            <HeaderCell>ID</HeaderCell>
-                            <HeaderCell>Description</HeaderCell>
-                            <HeaderCell>Amount</HeaderCell>
-                            <HeaderCell>Recipient</HeaderCell>
-                            <HeaderCell>Approval Count</HeaderCell>
-                            <HeaderCell>Approve</HeaderCell>
-                            <HeaderCell>Finalize</HeaderCell>
-                        </Row>
-                    </Header>
-                    <Body>
-                        { this.renderRows() }
-                    </Body>
-                </Table>
-                <div>Found { this.props.requestsCount } requests.</div>
-            </Layout>            
-        );
-    }
+    return (
+      <Layout>
+        <h3>Requests</h3>
+        <Link route={`/campaigns/${this.props.address}/requests/new`}>
+          <a>
+            <Button primary floated="right" style={{ marginBottom: 10 }}>
+              Add Request
+            </Button>
+          </a>
+        </Link>
+        <Table>
+          <Header>
+            <Row>
+              <HeaderCell>ID</HeaderCell>
+              <HeaderCell>Description</HeaderCell>
+              <HeaderCell>Amount</HeaderCell>
+              <HeaderCell>Recipient</HeaderCell>
+              <HeaderCell>Approval Count</HeaderCell>
+              <HeaderCell>Approve</HeaderCell>
+              <HeaderCell>Finalize</HeaderCell>
+            </Row>
+          </Header>
+          <Body>{this.renderRows()}</Body>
+        </Table>
+        <div>Found {this.props.requestCount} requests.</div>
+      </Layout>
+    );
+  }
 }
 
 export default RequestIndex;
